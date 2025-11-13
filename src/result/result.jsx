4a1416a3209986a168a495ab6083cd600fb48967
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2025-11-13 20:25:29 +0300

    Update result.jsx

--- src/result/result.jsx
+++ src/result/result.jsx
@@ -1,57 +1,47 @@
 import { useEffect } from "react";
 
-function Result({ bill, setBill, tip, setTip, number, setNumber,amount,setAmount,total, setTotal }){
+function Result({ bill, setBill, tip, setTip, number, setNumber, amount, setAmount, total, setTotal }) {
 
-   function tipAmount(){
+  useEffect(() => {
     const tipValue = parseFloat(tip) / 100;
-    const result = (bill * tipValue) / number; 
+    const result = (bill * tipValue) / number;
     setAmount(result.toFixed(2));
-   }
 
-   function totalResult(){
-    const totalr = Number(bill/number) + Number(amount);
+    const totalr = Number(bill / number) + Number(result);
     setTotal(totalr.toFixed(2));
-   }
+  }, [tip, bill, number, setAmount, setTotal]);
 
-   
-   useEffect(()=>{
-     tipAmount()
-     totalResult()
-   },[tip,bill,number,amount,total, tipAmount, totalResult]);
-
-   function resetButton(){
+  function resetButton() {
     setBill('');
     setNumber('1');
     setTip('5');
     setAmount('0.00');
     setTotal('0.00');
-   }
+  }
 
-    return(
-        <div className="result">
-            <div className="TipAmount">
-                <div>
-                    <label>Tip Amount</label>
-                    <h6>/ person</h6>
-                </div>
-                <div>
-                    <span>{amount}</span>
-                </div>
-            </div>
-            <div className="Total">
-                <div>
-                    <label>Total</label>
-                    <h6>/ person</h6>
-                </div>
-                <div>
-                    <span>{total}</span>
-                </div>
-            </div>
-            <button className="reset" onClick={resetButton}>RESET</button>
+  return (
+    <div className="result">
+      <div className="TipAmount">
+        <div>
+          <label>Tip Amount</label>
+          <h6>/ person</h6>
         </div>
-    )
+        <div>
+          <span>{amount}</span>
+        </div>
+      </div>
+      <div className="Total">
+        <div>
+          <label>Total</label>
+          <h6>/ person</h6>
+        </div>
+        <div>
+          <span>{total}</span>
+        </div>
+      </div>
+      <button className="reset" onClick={resetButton}>RESET</button>
+    </div>
+  );
 }
 
-
 export default Result;
-
